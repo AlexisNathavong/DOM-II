5d51cd7b46d690cdfa987c6da8002fe218708608
Alexis Panyathong <alexispanyathong@gmail.com>
2019-06-18 18:37:48 -0500

    Rearranged and cleaned up my work.

--- js/index.js
+++ js/index.js
@@ -34,86 +34,93 @@ window.addEventListener("keydown", function(event) {
     if (event.key == "f") {
         window.open ("https://mastery.games/p/flexbox-zombies", '_blank'
         )};
-});
-
-
-let funBusImg = document.querySelector(".intro img")
-
-funBusImg.addEventListener("dblclick", function(event) {
-  event.preventDefault();
-  funBusImg.style.width = "50%";
-});
-console.log(funBusImg);
-
-let adventureAwaitsTexts = document.querySelectorAll('.inverse-content .text-content p')
-adventureAwaitsTexts.forEach(paragraph => {
-    paragraph.addEventListener("click", function(event){
-        event.preventDefault();
-        paragraph.style.color = "rebeccapurple";
     });
+    
+/* WHEEL */
+let scrolling = true;
+    
+window.addEventListener("wheel", function(event) {
+    event.preventDefault();
+    body.style.background = "#ffcc00";
+        
+    if(scrolling) {
+        body.style.color = getRandomColor();
+        scrolling = false;
+        setTimeout(function() {
+        scrolling = true;
+    }, 100 )
+        }
 });
-
+    
+/* DRAGSTART & DRAGEND */
+body.addEventListener("dragstart", function(event) {
+    // event.preventDefault();
+    console.log(event.target);
+    event.target.style.opacity = "0.3";
+    });
+    
+body.addEventListener("dragend", function(event) {
+    // event.preventDefault();
+    console.log(event.target);
+    event.target.style.opacity = "";
+    });
+    
+/* LOAD */
+window.addEventListener("load", function(event) {
+    event.preventDefault();
+    alert("Page reloaded!");
+});
+    
+/* COPY */
 adventureAwaitsTexts.forEach(paragraph => {
     paragraph.addEventListener("copy", function(event) {
         event.preventDefault();
         alert("Please DO NOT COPY!");
-    });
+        });
 });
-
+    
+/* RESIZE */
+window.addEventListener("resize", function(event) {
+    event.preventDefault();
+    body.style.opacity = "1";
+});
+    
+/* SCROLL */
 let body = document.querySelector("body")
 let opacityCheck;
-
+    
 window.addEventListener("scroll", function(event) {
     event.preventDefault();
     clearTimeout(opacityCheck);
     body.style.opacity = "0.6";
     opacityCheck = setTimeout(function() {
-        body.style.opacity = "";
+    body.style.opacity = "";
     }, 66);
 });
-
-window.addEventListener("resize", function(event) {
-    event.preventDefault();
-    body.style.opacity = "1";
-});
-
-window.addEventListener("load", function(event) {
-    event.preventDefault();
-    this.alert("Page reloaded!");
-});
-
-body.addEventListener("dragstart", function(event) {
-    // event.preventDefault();
-    console.log(event.target);
-    event.target.style.opacity = "0.3";
-});
-
-body.addEventListener("dragend", function(event) {
-    // event.preventDefault();
-    console.log(event.target);
-    event.target.style.opacity = "";
-});
-
-let scrolling = true;
-
-window.addEventListener("wheel", function(event) {
-    event.preventDefault();
-    body.style.background = "#ffcc00";
     
-    if(scrolling) {
-        body.style.color = getRandomColor();
-        scrolling = false;
-        setTimeout(function() {
-            scrolling = true;
-        }, 100 )
-    }
+/* CLICK */
+let adventureAwaitsTexts = document.querySelectorAll('.inverse-content .text-content p')
+    
+adventureAwaitsTexts.forEach(paragraph => {
+    paragraph.addEventListener("click", function(event){
+    event.preventDefault();
+    paragraph.style.color = "rebeccapurple";
+    });
 });
 
-
- 
+/* DBLCLICK */
+let funBusImg = document.querySelector(".intro img")
+    
+funBusImg.addEventListener("dblclick", function(event) {
+    event.preventDefault();
+    funBusImg.style.width = "50%";
+    });
+console.log(funBusImg);
+    
+/* NESTED */
 let contentPickTexts = document.querySelector(".content-pick p")
 
 contentPickTexts.addEventListener("wheel", function(event) {
     event.stopPropagation();
     body.style.color = "navy";
-})
+});
